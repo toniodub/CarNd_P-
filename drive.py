5059111dc3b838096303fc69cb7584b421b0785e
toniodub <toniodub@gmail.com>
2017-01-11 16:11:01 -0800

    test3

--- drive.py
+++ drive.py
@@ -17,7 +17,7 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 
 # Fix error with Keras and TensorFlow
 #Test to see if git commit branch work
-#deuxieme test
+#troisieme test
 import tensorflow as tf
 tf.python.control_flow_ops = tf
 
