49d5f3b6dc596d1f3148332e4c82a71b8e966137
toniodub <toniodub@gmail.com>
2017-01-20 10:23:02 -0800

    adding saving model

--- model.py
+++ model.py
@@ -173,12 +173,12 @@ else:
 datagen.fit(X_train)
 
 print('start training')
-nb_epoch=5
+nb_epoch=1
 batch_size=256
 
 val_gen=datagen.flow(X_val, steering_val, batch_size=batch_size)
 
-for steering_th in range(30,0,-5):
+for steering_th in range(30,25,-5):
 	print('steering threshold:')
 	print(steering_th/100)
 	[X_train_temp, steering_train_temp]=steering_filtering(X_train,steering_train,steering_th/100)
@@ -186,7 +186,7 @@ for steering_th in range(30,0,-5):
 	train_gen=datagen.flow(X_train_temp, steering_train_temp, batch_size=batch_size)
 
 	# fits the model on batches with real-time data augmentation:
-	model.fit_generator(train_gen, samples_per_epoch=min(len(X_train_temp),2000), nb_epoch=nb_epoch)
+	model.fit_generator(train_gen, samples_per_epoch=min(len(X_train_temp),20000), nb_epoch=nb_epoch)
 	gc.collect()
 
 print()
