991b73bcf2415af3a959ffbf7948f14f2e17defd
toniodub <toniodub@gmail.com>
2017-01-19 16:25:52 -0800

    changing bug flip

--- model.py
+++ model.py
@@ -171,7 +171,7 @@ for steering_th in range(30,0,-5):
 	train_gen=datagen.flow(X_train_temp, steering_train_temp, batch_size=batch_size)
 
 	# fits the model on batches with real-time data augmentation:
-	history=model.fit_generator(train_gen, samples_per_epoch=max(len(X_train_temp),30000), 	nb_epoch=1,validation_data=val_gen, nb_val_samples=500)
+	history=model.fit_generator(train_gen, samples_per_epoch=min(len(X_train_temp),30000), 	nb_epoch=1,validation_data=val_gen, nb_val_samples=500)
 
 print()
 
